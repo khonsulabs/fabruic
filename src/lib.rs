77b906f2c8bf6738974d7ce3c15d669e3483145a
daxpedda <daxpedda@gmail.com>
2021-04-04 19:33:11 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+#![deny(unsafe_code)]
+#![warn(
+	clippy::cargo,
+	clippy::nursery,
+	clippy::pedantic,
+	clippy::restriction,
+	future_incompatible,
+	rust_2018_idioms
+)]
+#![warn(
+	box_pointers,
+	macro_use_extern_crate,
+	meta_variable_misuse,
+	missing_copy_implementations,
+	missing_crate_level_docs,
+	missing_debug_implementations,
+	missing_docs,
+	non_ascii_idents,
+	single_use_lifetimes,
+	trivial_casts,
+	trivial_numeric_casts,
+	unaligned_references,
+	unreachable_pub,
+	unused_import_braces,
+	unused_lifetimes,
+	unused_qualifications,
+	unused_results,
+	variant_size_differences
+)]
+#![allow(
+	clippy::blanket_clippy_restriction_lints,
+	clippy::else_if_without_else,
+	clippy::exhaustive_enums,
+	clippy::future_not_send,
+	clippy::implicit_return,
+	clippy::missing_inline_in_public_items,
+	clippy::non_ascii_literal,
+	clippy::pattern_type_mismatch,
+	clippy::redundant_pub_crate,
+	clippy::shadow_reuse,
+	clippy::tabs_in_doc_comments,
+	clippy::unreachable,
+	clippy::wildcard_enum_match_arm,
+	unreachable_pub,
+	variant_size_differences
+)]
+#![allow(clippy::cargo_common_metadata)]
+#![cfg_attr(
+	doc,
+	warn(rustdoc::all),
+	allow(rustdoc::missing_doc_code_examples, rustdoc::private_doc_tests)
+)]
+
+//! TODO
