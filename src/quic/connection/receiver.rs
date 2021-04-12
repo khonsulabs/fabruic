45174c3d67f8938a1c014d041852bf39115f08c9
daxpedda <daxpedda@gmail.com>
2021-04-12 16:02:43 +0200

    Re-introduce `read_chunk`

--- src/quic/connection/receiver.rs
+++ src/quic/connection/receiver.rs
@@ -3,15 +3,17 @@
 use std::{
 	convert::TryFrom,
 	fmt::{self, Debug, Formatter},
+	marker::PhantomData,
 	mem::size_of,
 	pin::Pin,
 	task::{Context, Poll},
 };
 
-use bytes::{Buf, BufMut, Bytes, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 use futures_channel::oneshot;
-use futures_util::{stream::Stream, StreamExt};
-use quinn::{Chunk, VarInt};
+use futures_util::{stream::Stream, FutureExt, StreamExt};
+use pin_project::pin_project;
+use quinn::{crypto::rustls::TlsSession, Chunk, VarInt};
 use serde::de::DeserializeOwned;
 
 use super::Task;
@@ -55,42 +57,26 @@ impl<T> Receiver<T> {
 		let task = Task::new(
 			async move {
 				/// Help Group Messages
-				enum Message {
+				enum Message<T> {
 					/// Data arrived from stream.
-					Data(Bytes),
+					Data(T),
 					/// [`Receiver`] asked to close.
 					Close,
 				}
 
-				let mut reader = stream.read_chunk(size_of::<u64>(), true);
+				let mut reader = ReceiverStream::new(&mut stream);
 				let mut shutdown = shutdown_receiver;
 
-				let mut length = 0;
-				// 1480 bytes is a default MTU size configured by quinn-proto
-				let mut data = BytesMut::with_capacity(1480);
-
 				while let Some(message) = allochronic_util::select! {
-					message: &mut reader => {
-						message
-							.map_err(Error::Read)?
-							.map(|Chunk { bytes, .. }| Message::Data(bytes))
-					},
+					message: &mut reader => message.transpose()?.map(Message::Data),
 					shutdown: &mut shutdown => shutdown.ok().map(|_| Message::Close),
 				} {
 					match message {
-						Message::Data(bytes) => {
-							let (data, length) =
-								Self::read_internal(&mut data, &mut length, bytes)?;
-
-							if let Some(data) = data {
-								// the receiver might have been dropped
-								if sender.send(data).is_err() {
-									break;
-								}
+						Message::Data(message) => {
+							// the receiver might have been dropped
+							if sender.send(message).is_err() {
+								break;
 							}
-
-							// `read_internal` calculated how much we need to read next
-							reader = stream.read_chunk(length, true);
 						}
 						Message::Close => {
 							stream
@@ -107,92 +93,6 @@ impl<T> Receiver<T> {
 		);
 
 		Self { receiver, task }
-	}
-
-	/// Reads messages from the stream. Calculates the amount needed to read for
-	/// the next message. Returns [`Some`] if a message has completed.
-	///
-	///  # Errors
-	/// [`Error::Deserialize`] if a message failed to be
-	/// [`Deserialize`](serde::Deserialize)d.
-	// TODO: update Clippy
-	#[allow(clippy::clippy::panic_in_result_fn, clippy::unwrap_in_result)]
-	fn read_internal<A: DeserializeOwned>(
-		data: &mut BytesMut,
-		length: &mut usize,
-		bytes: Bytes,
-	) -> Result<(Option<A>, usize)> {
-		// reserves enough space to put in incoming bytes
-		data.reserve(bytes.len());
-		data.put(bytes);
-
-		// if we don't have a length already
-		if *length == 0 {
-			// TODO: fix Clippy
-			#[allow(clippy::panic)]
-			{
-				debug_assert!(
-					data.len() <= size_of::<u64>(),
-					"received more data then requested"
-				);
-			}
-
-			// and there is enough to aquire it
-			#[allow(clippy::expect_used)]
-			if data.len() == size_of::<u64>() {
-				// aquire the length by reading the first 8 bytes (u64)
-				*length = usize::try_from(data.get_uint_le(size_of::<u64>()))
-					.expect("not a 64-bit system");
-				// demand the amount we need
-				Ok((None, *length))
-			}
-			// or we don't have enough data to complete 8 bytes (u64)
-			else {
-				// reduce the next amount to be read to what we need to reach 8
-				// bytes (u64)
-				Ok((
-					None,
-					size_of::<u64>()
-						.checked_sub(data.len())
-						.expect("wrong u64 length"),
-				))
-			}
-		}
-		// if we have a length
-		else {
-			// TODO: fix Clippy
-			#[allow(clippy::panic)]
-			{
-				debug_assert!(data.len() <= *length, "received more data then requested");
-			}
-
-			// and the data we gathered is enough
-			if data.len() == *length {
-				// split of the correct amoutn of data from what we have
-				// gathered until now
-				let data = data.split_to(*length).reader();
-				// reset the length so the condition above works again
-				*length = 0;
-
-				// deserialize data
-				// TODO: configure bincode, for example make it bounded
-				#[allow(box_pointers)]
-				let data = bincode::deserialize_from(data).map_err(|error| Error::Deserialize(*error))?;
-
-				// we are done reading, let's get the next length
-				Ok((Some(data), size_of::<u64>()))
-			}
-			// or the data is not long enough
-			else {
-				// reduce the next amount to be read to what we need to the data
-				// length
-				#[allow(clippy::expect_used)]
-				Ok((
-					None,
-					length.checked_sub(data.len()).expect("wrong data length"),
-				))
-			}
-		}
 	}
 
 	/// Wait for the [`Receiver`] part of the stream to finish gracefully.
@@ -223,5 +123,140 @@ impl<T> Stream for Receiver<T> {
 
 	fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
 		self.receiver.poll_next_unpin(cx)
+	}
+}
+
+/// Wrapper around [`RecvStream`](quinn::RecvStream) to implement framing.
+#[pin_project]
+struct ReceiverStream<'s, T: DeserializeOwned> {
+	/// Store length of the currently processing message.
+	length: usize,
+	/// Store incoming chunks.
+	buffer: BytesMut,
+	/// [`Quinn`](quinn)s receiver.
+	reader: quinn::generic::ReadChunk<'s, TlsSession>,
+	/// Type to be [`Deserialize`](serde::Deserialize)d
+	_type: PhantomData<T>,
+}
+
+impl<'s, T: DeserializeOwned> ReceiverStream<'s, T> {
+	/// Builds a new [`ReceiverStream`].
+	fn new(stream: &'s mut quinn::RecvStream) -> Self {
+		Self {
+			length: 0,
+			// 1480 bytes is a default MTU size configured by quinn-proto
+			buffer: BytesMut::with_capacity(1480),
+			reader: stream.read_chunk(usize::MAX, true),
+			_type: PhantomData,
+		}
+	}
+
+	/// [`Poll`](std::future::Future::poll)s [`RecvStream`](quinn::RecvStream)
+	/// for the next [`Chunk`] and stores it in [`ReceiverStream`]. Returns
+	/// [`None`] if the [`Stream`] is finished.
+	///
+	/// # Errors
+	/// [`Error::Read`] if the [`Receiver`] failed to read from the
+	/// [`RecvStream`](quinn::RecvStream).
+	fn poll(&mut self, cx: &mut Context<'_>) -> Poll<Result<Option<()>>> {
+		self.reader
+			.poll_unpin(cx)
+			.map_ok(|option| {
+				option.map(|Chunk { bytes, .. }| {
+					// reserves enough space to put in incoming bytes
+					self.buffer.reserve(bytes.len());
+					self.buffer.put(bytes);
+				})
+			})
+			.map_err(Error::Read)
+	}
+
+	/// Check if we currently have enough data to build
+	/// [`length`](Self::length) and returns it. Returns [`None`] if there isn't
+	/// enough data to extract [`length`](Self::length) yet.
+	#[allow(clippy::unwrap_in_result)]
+	fn length(&mut self) -> Option<usize> {
+		if self.length == 0 {
+			if self.buffer.len() >= size_of::<u64>() {
+				#[allow(clippy::expect_used)]
+				{
+					// aquire the length by reading the first 8 bytes (u64)
+					self.length = usize::try_from(self.buffer.get_uint_le(size_of::<u64>()))
+						.expect("not a 64-bit system");
+				}
+
+				Some(self.length)
+			} else {
+				None
+			}
+		} else {
+			Some(self.length)
+		}
+	}
+
+	/// [`Deserialize`](serde::Deserialize)s the currents
+	/// [`bufer`](Self::buffer) with the given `length`. Returns [`None`] if
+	/// there isn't enough data to extract [`length`](Self::length) yet.
+	///
+	/// # Errors
+	/// [`Error::Deserialize`] if `data` failed to be
+	/// [`Deserialize`](serde::Deserialize)d.
+	fn deserialize(&mut self, length: usize) -> Result<Option<T>> {
+		if self.buffer.len() >= length {
+			// split off the correct amount of data
+			let data = self.buffer.split_to(length).reader();
+			// reset the length
+			self.length = 0;
+
+			// deserialize message
+			// TODO: configure bincode, for example make it bounded
+			#[allow(box_pointers)]
+			bincode::deserialize_from::<_, T>(data)
+				.map(Some)
+				.map_err(|error| Error::Deserialize(*error))
+		} else {
+			Ok(None)
+		}
+	}
+}
+
+impl<T: DeserializeOwned> Stream for ReceiverStream<'_, T> {
+	type Item = Result<T>;
+
+	fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+		use futures_util::ready;
+
+		// do we have enough data to extract the length?
+		if let Some(length) = self.length() {
+			// did we receive enough data to deserialize the message?
+			if let Some(message) = self.deserialize(length)? {
+				// send back the message
+				Poll::Ready(Some(Ok(message)))
+			}
+			// try to poll for more data
+			else if ready!(self.poll(cx)?).is_some() {
+				// did we receive enough data to deserialize the message?
+				self.deserialize(length)?
+					.map_or(Poll::Pending, |message| Poll::Ready(Some(Ok(message))))
+			}
+			// stream has ended
+			else {
+				Poll::Ready(None)
+			}
+		}
+		// try to poll for more data
+		else if ready!(self.poll(cx)?).is_some() {
+			// did we receive enough data to extract the length?
+			if let Some(length) = self.length() {
+				self.deserialize(length)?
+					.map_or(Poll::Pending, |message| Poll::Ready(Some(Ok(message))))
+			} else {
+				Poll::Pending
+			}
+		}
+		// stream has ended
+		else {
+			Poll::Ready(None)
+		}
 	}
 }
