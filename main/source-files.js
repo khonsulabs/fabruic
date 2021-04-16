609509c2537012000d9ee4960d82901a393cb101
kl-botsu <botsu@khonsulabs.com>
2021-04-16 10:24:52 +0000

    Deploying to gh-pages from @ khonsulabs/fabruic@47f16e10720f4354beb010497caa2a99166dab65 🚀

--- main/source-files.js
+++ main/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["fabruic"] = {"name":"","dirs":[{"name":"quic","dirs":[{"name":"connection","files":["connecting.rs","incoming.rs","mod.rs","receiver.rs","receiver_stream.rs","sender.rs"]},{"name":"endpoint","dirs":[{"name":"builder","files":["config.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["mod.rs","task.rs"]}],"files":["certificate.rs","error.rs","lib.rs"]};
+sourcesIndex["fabruic"] = {"name":"","dirs":[{"name":"quic","dirs":[{"name":"connection","files":["connecting.rs","incoming.rs","mod.rs","receiver.rs","receiver_stream.rs","sender.rs"]},{"name":"endpoint","dirs":[{"name":"builder","files":["config.rs","mod.rs"]}],"files":["mod.rs"]}],"files":["mod.rs","task.rs"]}],"files":["certificate.rs","dangerous.rs","error.rs","lib.rs"]};
 createSourceSidebar();
