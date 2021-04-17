13be1d23d0bd56ac099ef09468e953ca1e727ac9
kl-botsu <botsu@khonsulabs.com>
2021-04-17 11:33:50 +0000

    Deploying to gh-pages from @ khonsulabs/fabruic@62371c97217c2176eb544e4b2e2b4ce1fc36f20f 🚀

--- main/fabruic/dangerous/sidebar-items.js
+++ main/fabruic/dangerous/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Builder","Security-sensitive configuration for [`Builder`]."],["Certificate","Gives read access to the [`PrivateKey`]."]]});
+initSidebarItems({"trait":[["Builder","Security-sensitive configuration for [`Builder`]."],["KeyPair","Gives serialization access to [`KeyPair`]."],["PrivateKey","Gives read access to the [`PrivateKey`]."]]});
