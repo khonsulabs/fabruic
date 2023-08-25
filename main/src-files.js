13f5c7df20fd703a82487a3af0f53c6f021383e3
kl-botsu <botsu@khonsulabs.com>
2023-08-25 14:29:56 +0000

    Deploying to gh-pages from @ khonsulabs/fabruic@f1c5c5b99f15f0371aceaa45a3252aac25442ec3 🚀

--- main/src-files.js
+++ main/src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "fabruic":["",[["quic",[["connection",[],["connecting.rs","incoming.rs","mod.rs","receiver.rs","receiver_stream.rs","sender.rs"]],["endpoint",[["builder",[],["config.rs","mod.rs"]]],["mod.rs"]]],["mod.rs","task.rs"]],["x509",[],["certificate.rs","certificate_chain.rs","mod.rs","private_key.rs"]]],["error.rs","lib.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
